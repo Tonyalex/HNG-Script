802ce903d7001bba94cfe68f74df7a23ca1333fc
Tonyalex <tonyalex@gmail.com>
2020-06-03 10:15:31 +0100

    Name(Nsikak_Akpan.java)

---
+++ Nsikak_Akpan.java
@@ -0,0 +1,16 @@
+package com.example.java;
+
+public class Nsikak_Akpan {
+
+        public static void main(String[] args) {
+        System.out.println("Hello World, my name is Nsikak Akpan, my HNGi7 ID is , HNG-01250, and i would be using java for my stage 2 task");
+            }
+
+
+        }
+
+
+
+
+
+
